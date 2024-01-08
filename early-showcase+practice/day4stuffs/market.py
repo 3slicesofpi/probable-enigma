e0cc6c95318981f9a723a03f72a71b6d9b7b824d
3slicesofpi <achunkoftau@gmail.com>
2024-01-08 16:27:31 +0800

    the market

---
+++ early-showcase+practice/day4stuffs/market.py
@@ -0,0 +1,51 @@
+def uc(num):
+    print('uc'+str(num))
+
+catalog = {'A1':['beans',5],'A2':['big beans',9],'A3':['coconut',3],'A4':['paper',29],'B1':['chocolate',2]}
+
+clientCat = {}
+
+def clientMenuSelect():
+    insert = str(input('> '))
+    if insert == 'dir':
+        dir()
+    elif insert == 'cat':
+        cat()
+    elif insert == 'cart':
+        cart()
+    elif insert == 'exit':
+        exit()
+    elif insert == 'take':
+        take()
+    elif insert == 'checkout':
+        checkout()
+    else:
+        help()
+
+
+def dir():
+    uc(1)
+    clientMenuSelect()
+
+def cat():
+    print(catalog)
+    clientMenuSelect()
+
+def take():
+    clientCat[len(list(clientCat))+1] = catalog[input('enter the code: > ')]
+    clientMenuSelect()
+
+def cart():
+    print(clientCat)
+    clientMenuSelect()
+
+def checkout():
+    print('your cart:')
+    print(clientCat)
+    print('total: ')
+    total = 0
+    for iteration in list(clientCat):
+        print(list(clientCat))
+        total += clientCat[iteration][1]
+    print(int(total))
+clientMenuSelect()
