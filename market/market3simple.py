950572b6b60b7d26d463557dfa5a7e9166305d84
3slicesofpi <achunkoftau@gmail.com>
2024-01-22 13:50:20 +0800

    minor addons

--- market/market3simple.py
+++ market/market3simple.py
@@ -1,6 +1,10 @@
 catalog = [{"product": "Black beans", "price": 5.98, "quantity": 4600}, {"product": "Kidney beans", "price": 4.98, "quantity": 9000}, {"product": "Chickpeas", "price": 6.58, "quantity": 13400}, {"product": "Chicken noodle soup", "price": 3.58, "quantity": 2400}, {"product": "Tomato soup", "price": 3.98, "quantity": 17800}, {"product": "Vegetable soup", "price": 4.58, "quantity": 6800}, {"product": "Tuna", "price": 2.98, "quantity": 11200}, {"product": "Canned salmon", "price": 7.98, "quantity": 15600}, {"product": "Canned sardines", "price": 5.58, "quantity": 18000}, {"product": "Canned vegetables (corn, peas, carrots)", "price": 5.38, "quantity": 24600}, {"product": "Canned peaches", "price": 5.98, "quantity": 11200}, {"product": "Canned pears", "price": 5.78, "quantity": 15600}, {"product": "Canned pineapple", "price": 6.98, "quantity": 9000}, {"product": "Spaghetti", "price": 2.58, "quantity": 6400}, {"product": "Penne", "price": 2.38, "quantity": 17800}, {"product": "White rice", "price": 4.98, "quantity": 4600}, {"product": "Brown rice", "price": 5.58, "quantity": 11200}, {"product": "Quinoa", "price": 9.98, "quantity": 15600}, {"product": "Oats", "price": 3.98, "quantity": 18000}, {"product": "Breakfast cereals", "price": 7.18, "quantity": 2400}, {"product": "Granola", "price": 7.58, "quantity": 6800}, {"product": "Lentils", "price": 4.78, "quantity": 9000}, {"product": "Split peas", "price": 4.38, "quantity": 13400}, {"product": "Pasta", "price": 2.98, "quantity": 4600}, {"product": "Rice", "price": 4.58, "quantity": 11200}, {"product": "Lentils", "price": 4.78, "quantity": 15600}, {"product": "Beans", "price": 4.58, "quantity": 18000}, {"product": "Crushed tomatoes", "price": 3.78, "quantity": 2400}, {"product": "Tomato sauce", "price": 2.98, "quantity": 9000}, {"product": "Tomato paste", "price": 2.58, "quantity": 13400}, {"product": "Flour", "price": 4.18, "quantity": 17800}, {"product": "Sugar", "price": 3.98, "quantity": 4600}, {"product": "Baking powder", "price": 3.58, "quantity": 11200}, {"product": "Baking soda", "price": 3.38, "quantity": 15600}, {"product": "Ketchup", "price": 4.98, "quantity": 18000}, {"product": "Mustard", "price": 3.58, "quantity": 2400}, {"product": "Mayonnaise", "price": 5.98, "quantity": 9000}, {"product": "Olive oil", "price": 8.98, "quantity": 13400}, {"product": "Vegetable oil", "price": 5.98, "quantity": 17800}, {"product": "Assorted Nuts", "price": 11.98, "quantity": 4600}, {"product": "Trail mix", "price": 7.78, "quantity": 11200}, {"product": "Crackers", "price": 4.58, "quantity": 15600}, {"product": "Popcorn", "price": 3.98, "quantity": 18000}, {"product": "Pasta sauce", "price": 4.78, "quantity": 2400}, {"product": "Salsa", "price": 5.18, "quantity": 9000}, {"product": "BBQ sauce", "price": 5.58, "quantity": 13400}, {"product": "Canned chicken", "price": 6.98, "quantity": 17800}, {"product": "Spam", "price": 5.98, "quantity": 4600}, {"product": "Corned beef", "price": 7.58, "quantity": 11200}]
 cart = []
 
+# todo hard: add squisher (removes duplicates) (use dict())
+# todo easy: add checkout
+# todo hard: add search-it
+
 
 from math import ceil 
 def listFormatter(theList): # this does well
@@ -50,6 +54,9 @@ def menuTakeItem():
     clientArgs = {'code':0,'quantity':0}
     print('please specify which item you want.')
     clientArgs['code'] = int(input('>>>:'))
+    if clientArgs["code"] > len(catalog):
+        print('Invalid product code! Try again.')
+        return
     print('how many of',catalog[clientArgs['code']]['product'],'do you want?')
     clientArgs['quantity'] = int(input('>>>:'))
     if clientArgs['quantity'] > (catalog[clientArgs['code']]['quantity']):
@@ -63,22 +70,31 @@ def menuTakeItem():
         print('You placed',cart[-1]['quantity'],cart[-1]['product'],'in your cart.')
     return
 
-def menuMain(): #return to this after every cycle.
-    # print("""
-    # welcome to the MAIN MENU
-    # To continue, press a command.
-
-    # 'l'/'lookcart': Look into your cart. 
-    # 'b'/'browsers': Browse our huge catalog.
-    # 'c'/'checkout': Purchase the items in your cart.
-    # 't'/'takeitem': Take a product from our catalog.
-    # 'r'/'returnit': Return a product from your cart.
-    # 'p'/'purgeall': Remove all items from your cart.
-    # 'h'/'helpshop': Get help.
-    # 'e'/'enditall': Exit the program.
+def menuHelp():
+    print('''
+    welcome to the HELP MENU
+    Basic Commands:
+     
+    'l'/'lookcart': Look into your cart. 
+    'b'/'browsers': Browse our huge catalog.
+    'c'/'checkout': Purchase the items in your cart.
+    't'/'takeitem': Take a product from our catalog.
+    'r'/'returnit': Return a product from your cart.
+    'p'/'purgeall': Remove all items from your cart.
+    'h'/'helpshop': Get help.
+    'e'/'enditall': Exit the program.
           
-    # Happy Shopping!
-    # """)
+    Happy Shopping!
+    
+    returning you to the main menu...
+    ''')
+    return
+
+def menuMain(): #return to this after every cycle.
+    print("""
+    welcome to the MAIN MENU
+    To continue, press a command or press 'h' for help.
+    """)
     clientInput = input('Input a command >>>:')
     if clientInput == '':
         print('No command given. Try again')
@@ -94,15 +110,19 @@ def menuMain(): #return to this after every cycle.
         menuTakeItem()
     elif clientInput[0] == 'r':
         return
-    elif clientInput[0] == 'p':
+    elif clientInput[0] == 'p': #bugged.
         cart = []
         print('your cart is now empty.')
         return
+    elif clientInput[0] == 'h':
+        menuHelp()
     elif (clientInput[0] == 'e') or (clientInput[0] == '&'):
         exit()
     else:
         print('Invalid Command. Try again.')
     return
+
+
 
 while True:
     menuMain()
