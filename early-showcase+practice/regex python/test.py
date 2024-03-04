1bbfc0eab34306bb2eb13eebfb2fe455a9f022ae
3slicesofpi <achunkoftau@gmail.com>
2024-03-04 12:39:02 +0800

    streamlined and improved regegame regegame 3

--- early-showcase+practice/regex python/test.py
+++ early-showcase+practice/regex python/test.py
@@ -1,5 +1,7 @@
 import re
 
-print(re.findall('[50871]','j'))
-o = 'a'
-print('bdeocbdeicde2',end=o)
+# print(re.findall('[50871]','j'))
+# o = 'a'
+# print('bdeocbdeicde2',end=o)
+
+print(len('cew'))
--- regegame/reggame prototype 2.py
+++ regegame/reggame prototype 2.py
@@ -18,6 +18,7 @@ def integratedFactoryAndValidator():
 
 def regexSectionConstructor(puzzleLength):
     puzzle = []
+    sectionLength = 0
     textFactoryMode = None
     for iteration in range(0,puzzleLength):
         match randint(1,4):
@@ -30,9 +31,11 @@ def regexSectionConstructor(puzzleLength):
                 for jteration in range(0,randint(1,5)):
                     puzzle.append(textFactory(textFactoryMode))
                 puzzle.append(']')
+                sectionLength += 1
             case 2:
                 # [a-c]
                 puzzle.append(integratedFactoryAndValidator())
+                sectionLength += 1
             case 3:
                 # [A-c]
                 puzzle.append('[')
@@ -42,6 +45,7 @@ def regexSectionConstructor(puzzleLength):
                 puzzle.append('-')
                 puzzle.append(textFactory(1))
                 puzzle.append(']')
+                sectionLength += 1
             case 4:
                 # a
                 textLength = randint(1,5)
@@ -49,18 +53,24 @@ def regexSectionConstructor(puzzleLength):
                     case 1:
                         for jteration in range(0,textLength):
                             puzzle.append(textFactory(randint(1,4)))
+                        sectionLength += textLength
                     case 2:
                         textLength += 1
                         puzzle.append('(')
                         for jteration in range(0,textLength):
                             puzzle.append(textFactory(randint(1,3)))
                         puzzle.append(')')
+                        sectionLength += textLength
                     case 3:
                         textLength += 1
                         puzzle.append('[')
                         for jteration in range(0,textLength):
                             puzzle.append(textFactory(randint(1,4)))
                         puzzle.append(']')
+                        if randint(1,2) ==2:
+                            sectionLength += textLength-1
+                        else:
+                            sectionLength += 2
         match randint(1,4):
             case 1:
                 match randint(1,4): 
@@ -68,19 +78,26 @@ def regexSectionConstructor(puzzleLength):
                         puzzle.append('+')
                     case 2:
                         puzzle.append('?')
+                        if randint(1,2) == 1:
+                            sectionLength -= 1
                     case 3:
                         puzzle.append('*')
+                        if randint(1,2) == 1:
+                            sectionLength -= 1
                     case 4:
                         puzzle.append('')
             case 2: # nothing
                 pass
             case 3:
                 puzzle.append('.')
+                sectionLength += 1
             case 4:
+                quantifier = randint(0,3)
                 puzzle.append('{')
-                puzzle.append(str(randint(0,3)))
+                puzzle.append(str(quantifier))
                 puzzle.append('}')
-    return puzzle
+                sectionLength += quantifier*sectionLength
+    return [puzzle, sectionLength]
 
 textTuple = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
 numberTuple = ('0','1','2','3','4','5','6','7','8','9')
@@ -111,75 +128,82 @@ def listJoinery(rawList):
         joinedString = joinedString+str(element)
     return joinedString
 
+def menuIntro():
+    print('blah')
+    try:
+        match input('>>>:').lower()[0]:
+            case 'e':
+                return 'menuExit'
+    except:
+        print('fail')
+        return 'menuExit'
+    return 'menuMain'
 
-# finalPuzzle = listJoinery(regexSectionConstructor(3))
-# print('/'+finalPuzzle+'/')
-# answer = input('answer>>>:')
-# if re.match(finalPuzzle,answer) != None:
-#     print('Correct!')
-# else:
-#     print('wrong. answer.')
-def gameEngine(menuState):
+def menuMain():
+    print('blah')
+    global difficulty
+    try:
+        match input('>>>:').lower()[0]:
+            case 'e':
+                return 'menuExit'
+            case '1':
+                difficulty = 1
+                return 'menuStart'
+            case '2':
+                difficulty = 2
+                return 'menuStart'
+    except:
+        print('fail')
+        return 'menuExit'
+
+def menuStart():
+    try:
+        numofRepeats = int(input('repeats >>>:'))
+        numofSections = int(input('sections >>>:'))
+    except:
+        print('fail')
+        numofRepeats = 0
+        numofSections = 0
+        return 'menuStart'
+    score = 0
+    for puzzleNum in range(1,numofRepeats):
+        print('puzzle no.'+str(puzzleNum))
+        theExpression = regexSectionConstructor(numofSections)
+        theExpression[0] = listJoinery(theExpression[0])
+        match difficulty:
+            case 1:
+                theExpression[1] = theExpression[1]*2 +3
+            case 2:
+                theExpression[1] += 3
+            case 3:
+                theExpression[1] += 1
+        print('/t Solve: /'+theExpression[0]+'/ in',theExpression[1],'charcters')
+        answer = input('/i Your Answer >>>:' )
+        score += 1
+        if re.match(theExpression[0],answer) != None:
+            print('Match! Your answer satisfies the regex.')
+        else:
+            print('Your answer does not satisfy the regex.')
+            score -= 1
+        if len(answer) > theExpression[1]:
+            print('Your answer was too long. It needs to be',theExpression[1],'long')
+            score -= 1
+    return 'menuMain'
+
+
+
+def mainLoop(menuState):
     match menuState:
-        case 'introMenu':
-            print('''
-/regular game/ is a game about solving regex puzzles. Are you up for the challenge?
-/n
-/n
-/i Press any key except 'e' to continue.
-                  ''')
-            if input('>>>:') == 'e':
-                return 'exit' 
-            else:
-                return 'startMenu'
-        case 'exit':
-            print('''
-/m exit
-/n
-/n
-/t Goodbye!
-                  ''')
+        case 'menuExit':
+            print('exit')
             exit()
-            print('/error exit() failed to end program. Try Again.')
-            return
-        case 'startMenu':
-            print('''
-/m start
-/n
-/c -game
-''')
-            gameLevelLength = int(input('/i select number of levels >>>:'))
-            print('/c &lvlen:',str(gameLevelLength))
-            gameExpressionLength = int(input('/i select length of expression >>>:'))
-            print('/c &exlen:',str(gameExpressionLength))
-            print('/n')
-            print('/t starting game...')
-            score = 0
-            for level in range(1,gameLevelLength):
-                print('/n')
-                print('/t',level)
-                theExpression = listJoinery(regexSectionConstructor(gameExpressionLength))
-                print('/t Solve: /'+theExpression+'/')
-                answer = input('/i Your Answer >>>:' )
-                if re.match(theExpression,answer) != None:
-                    print('Correct!')
-                    score += 1
-                else:
-                    print('wrong. answer.')
-            print('/t final score =',str(score)+'/'+str(gameLevelLength))
-            if score == gameLevelLength:
-                print('/t Matcher Maestro! Victory is yours this day.')
-            elif score == 0:
-                print('regex reject. Try again next time.')
-            else:
-                print('not bad. But you can do better.')
-            return 'startMenu'
+        case 'menuIntro':
+            return menuIntro()
+        case 'menuMain':
+            return menuMain()
+        case 'menuStart':
+            return menuStart()
 
-            
-
-
-menuState = 'introMenu'
+menuState = 'menuIntro'    
 while True:
-    menuState = gameEngine(menuState)
-
-
+    menuState = mainLoop(menuState)
