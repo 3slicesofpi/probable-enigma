9238754ca58f0e37ad3e8e4f266510a4ec81bb17
3slicesofpi <achunkoftau@gmail.com>
2024-03-26 12:49:15 +0800

    regegame 0.3.3 new gameSession, undergoing testing and tweaks to accomomodate argMode death/gamemode

--- early-showcase+practice/regex python/test.py
+++ early-showcase+practice/regex python/test.py
@@ -1,43 +1,9 @@
-import re
-from random import randint as rnd
-from random import getrandbits
-prevMenuVisited = 'menuStart'
+a = 1
+def funfunc():
+    a = 2
+    print(a)
+    return a
 
-def clientInput(raw):
-    match raw[0].lower():
-        case 'h':
-            return 'menuHelp'
-        case 'r':
-            return prevMenuVisited
-        case 'e':
-            return 'menuStart'
-        case 'd': # dont print
-            return 'menuDirect'
-        case 's': # dont print
-            return 'menuSessionSetup'
-        case 'q':
-            return 'menuQuit'
-        case _:
-            return raw
-
-    
-def menuStart():
-    print('''''')
-    command = clientInput(input('>>>:'))
-    if command.isnumeric():
-        match int(command):
-            case 1:
-                return 'menuClassic'
-            case 2:
-                return 'menuCustom'
-            case 3: 
-                return 'menuTutorial'
-            case 4:
-                return 'menuScenario'
-            case _:
-                print('Invaild Input: Command out of range. Try again.')
-                return prevMenuVisited
-    else:
-        return command
-    
-print(menuStart())
+print(a)
+print(funfunc())
+print(a)
---
+++ regegame/regegame 2.py/gameSession.py
@@ -0,0 +1,111 @@
+
+
+def regexSectionConstructor(numofSections):
+    return {'puzzle':'[a-z]','totalSections':1}
+
+argDict = { 
+    'penCorrect':True,'givenScore':0,
+    'givenTime':0,'randTime':0,'penTime':False,'randModeTime':0,
+    'givenChars':1,'randChars':0,'penChars':False,'randModeChars':1,
+    'numSections':3,'randSections':0,'randModeSections':2,
+    'numPuzzles':5,'sectionTime':2
+    }
+modeDict = {'gamemode':'classic','deathmode':'scoredown'}
+
+from time import time
+from random import randint as rnd
+import re
+def gameSession():
+    timeAnchorSession = time()
+    stats = []
+    #time, chars, sections
+    randNamesFloor = [0,0,0]
+    randNamesCeil = [0,0,0]
+    randNamesMode = (argDict['randModeTime'],argDict['randModeChars'],argDict['randModeSections'])
+    randNamesQuantity = (argDict['randTime'],argDict['randChars'],argDict['randSections'])
+    for randSelected in range(3):
+        if randNamesQuantity[randSelected]>0:
+            match randNamesMode[randSelected]:
+                case 0: # default
+                    randNamesFloor[randSelected] = -randNamesQuantity[randSelected]
+                    randNamesCeil[randSelected] = randNamesQuantity[randSelected]
+                case 1: # addonly
+                    # randNamesFloor[randSelected] = 0
+                    randNamesCeil[randSelected] = randNamesQuantity[randSelected]
+                case 2: # minusonly
+                    randNamesFloor[randSelected] = -randNamesQuantity[randSelected]
+                    # randNamesFloor[randSelected] = 0
+                case 3: # reduced minus
+                    randNamesFloor[randSelected] = 1-randNamesQuantity[randSelected]
+                    randNamesCeil[randSelected] = randNamesQuantity[randSelected]
+                case 4: # additional add
+                    randNamesFloor[randSelected] = -randNamesQuantity[randSelected]
+                    randNamesCeil[randSelected] = 1+randNamesQuantity[randSelected]
+                case 5: # reduced add
+                    randNamesFloor[randSelected] = -randNamesQuantity[randSelected]
+                    randNamesCeil[randSelected] = randNamesQuantity[randSelected]-1
+    puzzleNum = 0
+    if modeDict['gamemode'] == 'endless':
+        totalScore = 10
+        argDict['numPuzzles'] = 18446744073709551615
+    else:
+        totalScore = 0
+    killswitch = False
+    while (puzzleNum <= argDict['numPuzzles']):  #or killswitch):
+        if killswitch: # i guess python hates non-pythonic code?
+            # use this to sendure killswutch kills gameSession
+            return stats
+        puzzleNum += 1
+        print('puzzle no.:',puzzleNum)
+        # compute totals
+        totalSections = argDict['numSections']+rnd(randNamesFloor[2],randNamesCeil[2])
+        totalTime = argDict['givenTime']+rnd(randNamesFloor[0],randNamesCeil[0])+totalSections*argDict['sectionTime']
+        match modeDict['gamemode']:
+            # case 'scenario':
+            #     pass
+            case _:
+                theEx = regexSectionConstructor(totalSections)
+        totalChars = argDict['givenChars']+rnd(randNamesFloor[1],randNamesCeil[1])+theEx['totalSections']
+        if argDict['penTime']:
+            print('In',totalTime,'seconds,')
+        print('Solve /'+theEx['puzzle']+'/')
+        if argDict['penChars']:
+            print('using',totalChars,'characters')
+        timeAnchorLocal = time()
+        theAns = input('>>>:')
+
+        # count penalties and tally stats
+        localStats = {'score':argDict['givenScore']+1,'ansCorrect': True,'theAns':theAns,'theEx':theEx['puzzle'],'ansTime':time()-timeAnchorLocal,'totalTime':totalTime,'totalChars':totalChars}
+        if re.match(theEx['puzzle'],theAns) == None: #wrong
+            print('Incorrect Answer.')
+            localStats['ansCorrect'] = False
+            if argDict['penCorrect']:
+                localStats['score']-=2
+        if argDict['penTime']:
+            if localStats['ansTime']>=totalTime:
+                localStats['score']-=1
+                print('Exceeded Time Limit of',totalTime,'; You took',localStats['ansTime'],'seconds')
+        if argDict['penChars']:
+            if len(theAns)>totalChars:
+                localStats['score']-=1
+                print('Exceeded character Limit of',totalChars,'; Your answer was',len(theAns),'long.')
+        totalScore += localStats['score']
+        stats.append(localStats)
+        match modeDict['deathmode']:
+            case 'none':
+                pass
+            case 'scoredown':
+                if totalScore <=0:
+                    killswitch = True
+            case 'timedown':
+                timeAnchorSession += totalTime
+                if time()>timeAnchorSession:
+                    print('Out of time!')
+                    killswitch = True
+                else:
+                    print(-int(time()-timeAnchorSession)+argDict['givenTime']+(argDict['numSections']*argDict['sectionTime']))
+        
+    return stats
+        
+
+print(gameSession())
