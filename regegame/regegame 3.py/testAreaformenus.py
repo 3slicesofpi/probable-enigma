c5b95a72610b43daaaa4f5cf445688d0adf96897
3slicesofpi <achunkoftau@gmail.com>
2024-03-31 12:11:13 +0800

    i works!.jpg

--- regegame/regegame 3.py/testAreaformenus.py
+++ regegame/regegame 3.py/testAreaformenus.py
@@ -4,11 +4,11 @@ class menuConstructor():
         self.directory = {}
         # {key(etc: 1,2,H,Q): {'name':Monty's Menu,'goto':menuMonty},..}
     def addGlobalOptions(self):
-        self.directory['H'] = {'name':'Get Help','goto':'viewHelp'}
-        self.directory['R'] = {'name':'Return to Main Menu','goto':'menuMain'}
+        self.directory['H'] = {'name':'Get Help','goto':'viewHelp','type':'nav'}
+        self.directory['R'] = {'name':'Return to Main Menu','goto':'menuMain','type':'nav'}
         if goback != 'mainMenu':
-            self.directory['E'] = {'name':'Go back to previous menu','goto':goback}
-        self.directory['Q'] = {'name':'Quit Program','goto':'viewQuit'}
+            self.directory['E'] = {'name':'Go back to previous menu','goto':goback,'type':'nav'}
+        self.directory['Q'] = {'name':'Quit Program','goto':'viewQuit','type':'nav'}
 
     def addNewOptions(self,name,goto):
         # CALL THIS FIRST
@@ -34,9 +34,37 @@ class menuConstructor():
             # except:
             #     print('Invalid input! Try Again.')
 
-class mcqConstructor(menuConstructor):
-    # think over it
-    pass
+class gameArgsConstructor(menuConstructor):
+    # figure out how to set new target
+    def addNewOptions(self, name, goto):
+        # CALL THIS FIRST
+        self.directory[str(len(self.directory)+1)] = {'name':name,'goto':goto,'type':'nav'}
+    def addNewMultiChoice(self, name, goto):
+        # CALL THIS FIRST
+        self.directory[str(len(self.directory)+1)] = {'name':name,'goto':goto,'type':'mcq'}
+    def addNewTFToggle(self, name, goto):
+        # CALL THIS FIRST
+        self.directory[str(len(self.directory)+1)] = {'name':name,'goto':goto,'type':'tog'}
+    def addCustomOptions(self, name, pos, goto, type):
+        self.directory[pos] = {'name':name,'goto':goto,'type':type}
+    def inputHandler(self):
+        while True:
+            userInput = input('>>>:')[0].upper() 
+            for here in self.directory:
+                if userInput == here: # first iteration
+                    match self.directory[here]['type']:
+                        case 'nav':
+                            return self.directory[here]['goto']
+                        case 'mcq':
+                            global gameArgs
+                            try:
+                                gameArgs['goto'] = int(input('>>>:'))
+                            except:
+                                print('Invalid Input.')
+                        case 'tog':
+                            global gameArgs
+                            gameArgs['goto'] = (not gameArgs['goto'])
+    
 
 def menuMain():
     content = menuConstructor('menuMain')
@@ -56,7 +84,7 @@ def menuCustom():
     content.addNewOptions('Penalty Settings','mcqArgsPen')
     content.addNewOptions('Time Settings','mcqArgsTime')
     content.addNewOptions('Character Settings','mcqArgsChars')
-    content.addNewOptions('Difficulty Presets','mcqArgsPresets')
+    content.addNewOptions('Difficulty Presets','mcqClassic')
     content.addGlobalOptions()
     content.viewDirectory()
     global goto
@@ -95,6 +123,50 @@ Are you sure you want to exit this program?
     else:
         return goto
 
+def mcqClassic():
+    content = menuConstructor('mcqClassic')
+    content.addNewOptions('Easy Difficulty','1')
+    content.addNewOptions('Medium Difficulty','2')
+    content.addNewOptions('Hard Difficulty','3')
+    content.addNewOptions('Insane Difficulty','4')
+    content.addGlobalOptions()
+    content.viewDirectory()
+    inputCheck = content.inputHandler()
+    global goto
+    match inputCheck:
+        case '1':
+            global gameArgs
+            gameArgs = {}# blah blah blah
+            if goback == 'menuCustom': #coming from mcqPresets
+                goto = goback
+            else:
+                goto = 'gameSetup'
+        case '2':
+            global gameArgs
+            gameArgs = {}# blah blah blah
+            if goback == 'menuCustom': #coming from mcqPresets
+                goto = goback
+            else:
+                goto = 'gameSetup'
+        case '3':
+            global gameArgs
+            gameArgs = {}# blah blah blah
+            if goback == 'menuCustom': #coming from mcqPresets
+                goto = goback
+            else:
+                goto = 'gameSetup'
+        case '4':
+            global gameArgs
+            gameArgs = {}# blah blah blah
+            if goback == 'menuCustom': #coming from mcqPresets
+                goto = goback
+            else:
+                goto = 'gameSetup'
+        case _:
+            goto = inputCheck
+            
+        
+
 goback = 'menuMain'
 goto ='menuMain'
 while True:
@@ -110,9 +182,11 @@ while True:
         case 'menuCustom':
             menuCustom()
         case 'mcqGamemode':
-            mcqGamemode()
+            goto = goback
+            # mcqGamemode()
         case 'mcqTutorial':
-            mcqTutorial()
+            goto = goback
+            # mcqTutorial()
 
 
 
